b0045355e3663016e3c7235bbd9feada20bed68a
harish <guptaharish872@gmail.com>
2024-08-13 10:41:03 +0530

    initialize repo

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+
+app=Flask(__name__)
+
+client=MongoClient('localhost',27017) #creating client
+db=client.touchwoodgraphics # creating database
+service=db.services #creating collection
+service.insert_one({'name':'tshirt printing','price':'200'})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
