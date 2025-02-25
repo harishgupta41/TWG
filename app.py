86ab51f554f914db1cdec3a559a0204740f63a2e
harish <guptaharish872@gmail.com>
2025-02-25 23:52:43 +0530

    inserted images and styles

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template
-from pymongo import MongoClient
-import base64
 
 app=Flask(__name__)
-
-# client=MongoClient('localhost',27017) #creating client
-# db=client.touchwoodgraphics # creating database
-# service=db.services #creating collection
-# service.insert_one({'name':'tshirt printing','price':'200'})
 
 @app.route('/')
 @app.route('/home')
